33841d3e5471507962efcf5c7705b204b7bbff76
Vishnudivya2020 <146070060+Vishnudivya2020@users.noreply.github.com>
2023-12-04 22:14:47 +0300

    Add files via upload

---
+++ xyz/Resume.js
@@ -0,0 +1,32 @@
+//Resume data in JSON format.
+let Resume={
+    NAME:"DIVYABHARATHI",
+    AGE:27,
+    email:"vishnudivyabharathi@gmail.com",
+    MOBILE:8056745541,
+    DOB:15/10/1996,
+    ADDRESS:"AUNGAMUTHU STREET",
+    CITY:"PALANI",
+}
+console.log(JSON.stringify(Resume));
+
+let Education={
+    UG:"VIDHYASAGAR COLLEGE",
+    PERCENTAGE:80,
+    YEAR:(2014-2017),
+    PG:"GVG COLLEGE",
+    PERCENTAGE:82,
+    YEAR:(2017-2019),
+
+
+}
+console.log(JSON.stringify(Education));
+
+
+let Additionalinformation={
+    EXPERIENCE:"TEACHING",
+    SKILLS:"QUICKLEARNING,CREATIVEWORK",
+    OBJECTIVE:"SEEKING A CHALLENGING POSITION IN A REPUTED ORGANIZATION WHERE I CAN LEARN NEW SKILLS,EXPAND MY KNOWLEDAGE,AND LEVERAGE MY LEARNINGS.",
+
+}
+console.log(JSON.stringify(Additionalinformation));
